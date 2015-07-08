084b6ce44b13737115a6a37b5afe70e11df4f714
Ryan Buzzell <rbuzzell@csh.rit.edu>
2015-07-08 02:02:38 -0400

    Started using jquery 2.1.4

--- content.php
+++ content.php
@@ -83,8 +83,8 @@
  	<!-- Le javascript
 	 ================================================== -->
 	<!-- Placed at the end of the document so the pages load faster -->
-	<script src="js/jquery.js"></script>
-	<script src="./js/bootstrap.js"></script>
+	<script src="js/jquery-2.1.4.min.js"></script>
+	<script src="js/bootstrap.js"></script>
 
 
 	</body><style type="text/css"></style></html>
