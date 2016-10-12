b83ea03b269886573850d57ec67b28ec8ee194e6
Braden Bowdish <bmbowdish@csh.rit.edu>
2016-10-12 02:37:19 -0400

    Replaced Index.php with segfaults.php
    This now is slightly less ugly looking TM.

--- index.php
+++ index.php
@@ -1,67 +1,79 @@
 <!DOCTYPE html>
-<!-- saved from url=(0072)http://twitter.github.com/bootstrap/examples/starter-template.html#about -->
-<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-		<meta charset="utf-8">
-		<title>CSH Segfault</title>
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta name="description" content="Viewer for the CSH Segfault History">
-		<meta name="author" content="Ryan Buzzell">
+<html lang="en">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta charset="utf-8">
+        <title>CSH Segfault</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="CSH Segfault viewer">
+        <meta name="author" content="Ryan Buzzell">
         <link rel="icon" href="img/favicon.ico">
 
-    <!-- Le styles -->
-		<link href="css/bootstrap.css" rel="stylesheet">
+        <!-- Le styles -->
+        <link href="css/bootstrap.css" rel="stylesheet">
 
-		<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
-		<!--[if lt IE 9]>
-			<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-		<![endif]-->
+        <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+        <!--[if lt IE 9]>
+        <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <nav class="navbar navbar-inverse">
+            <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#" style="padding-left: 30px; padding-top: 10px;"><img src="img/csh_logo_white.svg"></a>
+                    <a class="navbar-brand" href="#">Segfault Archive</a>
+                </div>
 
-	</head>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        <li><a href="index.php">Home</a></li>
+                        <!--<li  class="active"><a href="#">Segfaults <span class="sr-only">(current)</span></a></li>-->
+                        <!--<li><a href="coredump.php">Coredump</a></li>-->
+                    </ul>
+                </div><!-- /.navbar-collapse -->
+            </div><!-- /.container-fluid -->
+        </nav>
 
-	<body>
-
-<nav class="navbar navbar-inverse">
-    <div class="container-fluid">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#" style="padding-left: 30px; padding-top: 10px;"><img src="img/csh_logo_white.svg"></a>
-            <a class="navbar-brand" href="#">Segfault Archive</a>
+        <div class="container-fluid">
+            <div class="row-fluid">
+                <div class="col-xs-2">
+                    <iframe src="content.php" frameborder="0" id="toc" width="100%"></iframe>
+                </div>
+                <div class="col-sm-10">
+                    <iframe src="segfaults/Volume%2001,%20Issue%2001.pdf" frameborder="0" name="pdfIFrame" id="pdfIFrame" width="100%"></iframe>
+                </div>
+            </div>
         </div>
 
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Home <span class="sr-only">(current)</span></a></li>
-                <li><a href="segfaults.php">Segfaults</a></li>
-                <!--<li><a href="coredump.php">Coredump</a></li>-->
-            </ul>
-        </div><!-- /.navbar-collapse -->
-    </div><!-- /.container-fluid -->
-</nav>
+        <!-- Le javascript
+         ================================================== -->
+        <!-- Placed at the end of the document so the pages load faster -->
+        <script src="js/jquery-2.1.4.min.js"></script>
+        <script src="js/jquery.responsiveiframe.js"></script>
+        <script src="js/bootstrap.js"></script>
+        <script>
+            $(window).load(function (){
+                $("#pdfIFrame").height(Math.floor($(window).height() * 0.9));
+                $("#toc").height(Math.floor($(window).height() * 0.9));
+            });
+        </script>
+        <script>
+            $(window).resize(function (){
+                $("#pdfIFrame").height(Math.floor($(window).height() * 0.9));
+                $("#toc").height(Math.floor($(window).height() * 0.9));
+            });
+        </script>
+    </body>
 
 
-<h1 style="text-align:center">Welcome to the CSH Segfault Archive</h1>
-	
-<div class="row">
-	<div class="col-xs-12">
-	    <p style="text-align:center">As a note: all available content has been added. Unadded content is missing content. If you have
-            access to any of the missing content, please email it to <a href="mailto:segfault@csh.rit.edu">segfault@csh.rit.edu</a>
-    </div>
-</div>
-        <!-- /container -->
 
-		<!-- Le javascript
-		================================================== -->
-		<!-- Placed at the end of the document so the pages load faster -->
-		<script src="js/jquery-2.1.4.min.js"></script>
-		<script src="js/bootstrap.js"></script>
-
-	
-
-</body><style type="text/css"></style></html>
+</html>
